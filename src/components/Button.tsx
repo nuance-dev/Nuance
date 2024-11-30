91777cb63202db2ced84c2368c4237ef5d97ac1d
Nua <06-boilers.brushy@icloud.com>
2024-11-30 12:48:18 -0300

    Fix invisible buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ function ButtonInner({
 }) {
   return (
     <>
-      <span className="absolute inset-0 rounded-md bg-gradient-to-b from-white/80 to-white opacity-10 transition-opacity group-hover:opacity-15" />
+      <span className="absolute inset-0 rounded-md bg-gradient-to-b from-white/80 to-white opacity-10 transition-opacity group-hover:opacity-15 dark:from-white/80 dark:to-white dark:opacity-10" />
       <span className="absolute inset-0 rounded-md opacity-7.5 shadow-[inset_0_1px_1px_white] transition-opacity group-hover:opacity-10" />
       {children} {arrow ? <span aria-hidden="true">&rarr;</span> : null}
     </>
@@ -28,7 +28,7 @@ export function Button({
 )) {
   className = clsx(
     className,
-    'group relative isolate flex-none rounded-md py-1.5 text-[0.8125rem]/6 font-semibold text-white',
+    'group relative isolate flex-none rounded-md py-1.5 text-[0.8125rem]/6 font-semibold text-gray-900 dark:text-white border border-gray-200 dark:border-white/10 bg-white/90 dark:bg-transparent',
     arrow ? 'pl-2.5 pr-[calc(9/16*1rem)]' : 'px-2.5',
   )
 
