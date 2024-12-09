3e7f101ad1817ba043236a7af7b7929a5708b9b2
Joaco <06-boilers.brushy@icloud.com>
2024-12-09 04:34:08 -0300

    Lazy

---
+++ src/components/LazyYouTube.tsx
@@ -0,0 +1,17 @@
+import dynamic from 'next/dynamic'
+
+const YouTubeEmbed = dynamic(() => import('./YouTubeEmbed'), {
+  loading: () => (
+    <div className="aspect-video w-full animate-pulse bg-gray-200 dark:bg-gray-800" />
+  ),
+  ssr: false,
+})
+
+interface LazyYouTubeProps {
+  videoId: string
+  title: string
+}
+
+export function LazyYouTube({ videoId, title }: LazyYouTubeProps) {
+  return <YouTubeEmbed videoId={videoId} title={title} />
+}
